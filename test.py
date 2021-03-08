ddb4e5b68b361d1f1bc06b93fe144cf71ef4a5a2
John T. Foster <john.foster@utexas.edu>
2021-03-08 09:12:14 -0600

    Fix unit tests

--- test.py
+++ test.py
@@ -34,22 +34,21 @@ with open("assignment11.py", "w") as f:
 from assignment11 import WellPlot
 
 class TestSolution(unittest.TestCase):
-    
+
     def test_plot(self):
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
-
             p = WellPlot('eog_wells_in_nd.csv')
             p.save_plot()
 
             gold_image = cv2.imread('images/eog_wells_in_nd_gold.png')
             test_image = cv2.imread('eog_wells_in_nd.png')
 
-            test_image_resized = skimage.transform.resize(test_image, 
-                                                          (gold_image.shape[0], gold_image.shape[1]), 
+            test_image_resized = skimage.transform.resize(test_image,
+                                                          (gold_image.shape[0], gold_image.shape[1]),
                                                           mode='constant')
 
-            ssim = skimage.measure.compare_ssim(skimage.img_as_float(gold_image), 
+            ssim = skimage.measure.compare_ssim(skimage.img_as_float(gold_image),
                                                 test_image_resized, multichannel=True)
             assert ssim >= 0.75
 
